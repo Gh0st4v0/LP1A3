e835a23ef1d8c2f83b54b3af353b4c22e901a801
Gh0st4v0 <89959988+Gh0st4v0@users.noreply.github.com>
2023-03-20 10:02:28 -0300

    Delete Atividade 1/LP1A3-main directory

--- Atividade 1/LP1A3-main/src/Animal.java
+++
@@ -1,8 +0,0 @@
-public abstract class Animal implements Alimentavel{
-    protected String nome;
-    protected int idade;
-    protected String especie;
-    protected boolean alimentado;
-
-    public abstract void emitirSom();
-}
